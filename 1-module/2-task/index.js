68982819f7bb7f4e67ef5e403765c42b82a0a314
maria <saprina.mariya@mail.ru>
2020-03-01 18:43:03 +0300

    module 1 task 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,3 +1,5 @@
+import { strict } from "assert";
+
 /**
  * Эту функцию трогать не нужно
  */
@@ -10,6 +12,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  return !!name && name.length >= 4 && name.indexOf(' ') === -1;
 }
 
 function sayHello() {
