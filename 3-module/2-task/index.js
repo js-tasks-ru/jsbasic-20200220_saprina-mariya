65843c7ade9025151874eb64502336813638a2bd
maria <saprina.mariya@mail.ru>
2020-03-25 13:48:50 +0300

    3 module 2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,22 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let array = str.split(' ');
+  for (let item of array) {
+    if (item.includes(',')) {
+        let result = item.split(',');
+
+        for (let subItem of result) {
+            array.push(subItem);
+        }
+    }
+  }
+  array = array
+    .map(item => parseFloat(item))
+    .filter(item => isFinite(item));
+
+    return {
+      'min': Math.min(...array),
+      'max': Math.max(...array)
+  }
 }
